e2a45a0c0a5b39852ac6295a328fcc9befe38ed2
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-11-13 15:59:57 +0000

    Update model2.py

--- model2.py
+++ model2.py
@@ -5,12 +5,45 @@ import scopy.ScoDruglikeness
 from dimorphite_dl import DimorphiteDL
 import streamlit as st
 
+import pickle
+import datamaker
+import streamlit as st
+st.title('Streamlit + RDKit :rocket:')
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem.Draw import SimilarityMaps
+from io import BytesIO
+from functools import partial
+from PIL import Image
+from rdkit.Chem.Draw import rdDepictor
+rdDepictor.SetPreferCoordGen(True)
+
 st.header('TC/L interaction probability model')
 st.caption("""Input a SMILES code of your molecule of choice.
 A probability for interaction with taurocholate/lecithin is computed based on two descriptors, logD and CrippenMR.
 The model is based on results from Mol. Pharmaceutics 2022, 19, 2868−2876, but descriptors are computed via rdkit/scopy instead of MOE/PaDEL.""")
 
-SMI = st.text_input('Enter Canonical SMILES of drug', 'CC(C)NCC(COC1=CC=C(C=C1)CCOC)O')
+import streamlit.components.v1 as components
+
+_RELEASE = False
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+            "chemstreamlit",
+            url="http://localhost:3001"
+            )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("my_component", path=build_dir)
+
+def my_component():
+    component_value = _component_func()
+    return component_value
+
+SMI = my_component()
+
+#SMI = st.text_input('Enter Canonical SMILES of drug', 'CC(C)NCC(COC1=CC=C(C=C1)CCOC)O')
 
 dimorphite_dl = DimorphiteDL(
     min_ph=6.5,
