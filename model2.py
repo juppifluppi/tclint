c5e592bb6412703c7ac16347d84ee708c1b836d3
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-11-13 16:27:29 +0000

    Update model2.py

--- model2.py
+++ model2.py
@@ -6,7 +6,7 @@ from dimorphite_dl import DimorphiteDL
 import streamlit as st
 
 st.header('TC/L interaction probability model')
-st.caption("""Input a SMILES code of your molecule of choice (use e.g. https://pubchem.ncbi.nlm.nih.gov//edit3/index.html).
+st.caption("""Input a SMILES code of your molecule of choice (use e.g. https://pubchem.ncbi.nlm.nih.gov/edit3/index.html).
 A probability for interaction with taurocholate/lecithin is computed based on two descriptors, logD and CrippenMR.
 The model is based on results from Mol. Pharmaceutics 2022, 19, 2868−2876, but descriptors are computed via rdkit/scopy instead of MOE/PaDEL.""")
 
