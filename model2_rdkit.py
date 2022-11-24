453778c7199e1d50d2251463de01ab16f1d9bd22
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-11-24 17:36:58 +0000

    Update model2_rdkit.py

--- model2_rdkit.py
+++ model2_rdkit.py
@@ -37,7 +37,7 @@ st.header('TC/L interaction probability model')
 st.caption("""Input a SMILES code of your molecule of choice (use e.g. https://pubchem.ncbi.nlm.nih.gov/edit3/index.html).
 A probability for interaction with taurocholate/lecithin is computed for the compound at pH 6.5, based on two descriptors: logD and CrippenMR.
 The model is based on Mol. Pharmaceutics 2022, 19, 2868−2876 (https://doi.org/10.1021/acs.molpharmaceut.2c00227),
-but rebuilt with corresponding descriptors from rdkit/scopy instead of MOE/PaDEL, using logD for pH 7.4 instead of 7.0.""")
+but was rebuilt with corresponding descriptors from rdkit/scopy instead of MOE/PaDEL, using logD for pH 7.4 instead of 7.0.""")
 
 st.caption("""A scatter plot shows the properties of the compound in relation to the training and the validation set. SDC applicability
 domain metrics based on the tanimoto similarity of the compound to the training set is given underneath (J. Chem. Inf. Model. 2019, 59, 181−189).
