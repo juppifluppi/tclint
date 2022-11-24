e254c563d4c9aeab2eb3e99f189a60ea4255666a
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-11-24 17:08:12 +0000

    Update model2_rdkit.py

--- model2_rdkit.py
+++ model2_rdkit.py
@@ -125,8 +125,8 @@ for k in o:
             pass
     lit.append(np.sum(lkx))
 
-st.write(str("Training set: "+np.min(lit)+" - "+np.max(lit))       
-st.write(str("Validation set: "+np.min(li)+" - "+np.max(li))   
+st.write("Training set: "+np.min(lit)+" - "+np.max(lit))       
+st.write("Validation set: "+np.min(li)+" - "+np.max(li))   
          
 fig=plt.figure()
 ax=fig.add_axes([0,0,1,1])
