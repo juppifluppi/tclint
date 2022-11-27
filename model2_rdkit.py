20e0aaa76e00207670f36473d71539480f563271
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-11-27 14:19:30 +0000

    Update model2_rdkit.py

--- model2_rdkit.py
+++ model2_rdkit.py
@@ -33,17 +33,17 @@ ox=[]
 for lines in fil2:
     ox.append(Chem.MolFromSmiles(lines))
 
-st.header('TC/L interaction probability model')
+st.title('TC/L interaction probability model')
 st.caption("""Input a SMILES code of a molecule (use e.g. https://pubchem.ncbi.nlm.nih.gov/edit3/index.html).
 A probability for interaction with taurocholate/lecithin is computed at pH 6.5, based on two descriptors: logD and CrippenMR.
 The model is inspired by Mol. Pharmaceutics 2022, 19, 2868−2876 (https://doi.org/10.1021/acs.molpharmaceut.2c00227)
 but was built with descriptors from rdkit/scopy instead of MOE/PaDEL, using logD for pH 7.4 instead of 7.0.
-For the same traning and validation sets similar statistics are retrieved (balanced accuracy: 0.86/0.83, AUC: 0.93/0.93).""")
+For the same traning/validation set similar statistics are retrieved (balanced accuracy: 0.86/0.83, AUC: 0.93/0.93).""")
 
 
 try:
 
-    SMI = st.text_input('Enter SMILES of drug molecule', 'CC(C)NCC(COC1=CC=C(C=C1)CCOC)O')
+    SMI = st.text_input('Enter SMILES', 'CC(C)NCC(COC1=CC=C(C=C1)CCOC)O')
     
     dimorphite_dl = DimorphiteDL(
         min_ph = 6.4,
@@ -82,13 +82,12 @@ domain metric based on the tanimoto similarity of the compound to the training s
 181−189). Higher SDC values and / or large distances to the training set in the plot can indicate less reliable predictions.""")
 
 
-#fp1 = Chem.RDKFingerprint(mol)
+
 fp1 = AllChem.GetMorganFingerprint(mol, 2)
 
 g=[]
 
 for molx in o:
-    #fp2 = Chem.RDKFingerprint(molx)
     fp2 = AllChem.GetMorganFingerprint(molx, 2)
     Tan = DataStructs.TanimotoSimilarity(fp1,fp2)
     try:
