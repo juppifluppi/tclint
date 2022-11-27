d5750a18e031a045855d92b296c8f14c20dfa857
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-11-27 19:55:32 +0000

    Update tclint_streamlit.py

--- tclint_streamlit.py
+++ tclint_streamlit.py
@@ -1,5 +1,5 @@
 ### Streamlit web app to compute TC/L interaction probability of compound
-### Version 1.0 (27.11.22): https://github.com/juppifluppi/tcli
+### Version 1.0 (27.11.22): https://github.com/juppifluppi/tclint
 
 from rdkit import Chem
 from rdkit import DataStructs
