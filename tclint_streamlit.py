b27d198fa9c00c7788cf8fc7e85d2e7db96b8fb8
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-11-29 20:33:13 +0000

    Update tclint_streamlit.py

--- tclint_streamlit.py
+++ tclint_streamlit.py
@@ -204,7 +204,7 @@ ax.set_title('Compound vs. modeling sets')
 
 for j in range(1,len(zlf)):
     if zlf[j] > 0.4999:
-        ax.scatter(dlf[j],clf[j],color="grey",alpha=0.05,s=80, marker= "D")
+        ax.scatter(dlf[j],clf[j],color="grey",alpha=0.08,s=50)
 
 for j in range(1,len(z)):
     if z[j] > 49.999:
