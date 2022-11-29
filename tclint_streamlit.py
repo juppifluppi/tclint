21c2eb6b98aefa090b09961b195c6d014ab15b44
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-11-29 19:37:28 +0000

    Update tclint_streamlit.py

--- tclint_streamlit.py
+++ tclint_streamlit.py
@@ -193,7 +193,7 @@ for k in o:
     
 fig=plt.figure()
 ax=fig.add_axes([0,0,1,1])
-ax1=fig.add_subplot(111)
+ax1=fig.add_subplots(111)
 ax.scatter(x, y, color='b',alpha=0.5,s=50)
 ax.scatter(x2, y2, color='r',alpha=0.5,s=50)
 ax.scatter(logd, mr, color='g',alpha=1,s=150,marker="*")
