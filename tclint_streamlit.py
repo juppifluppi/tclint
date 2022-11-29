fc4ee1658def7fdbc3659e222c3734b46a5aa7c5
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-11-29 19:45:17 +0000

    Update tclint_streamlit.py

--- tclint_streamlit.py
+++ tclint_streamlit.py
@@ -192,8 +192,8 @@ for k in o:
 # plot values
     
 fig=plt.figure()
-ax=fig.add_axes([0,0,1,1])
-ax1=fig.add_subplot(111)
+ax1=fig.add_axes([0,0,1,1])
+ax=fig.add_subplot(111)
 ax.scatter(x, y, color='b',alpha=0.5,s=50)
 ax.scatter(x2, y2, color='r',alpha=0.5,s=50)
 ax.scatter(logd, mr, color='g',alpha=1,s=150,marker="*")
@@ -202,6 +202,7 @@ ax.set_ylabel('CrippenMR')
 ax.set_title('Compound vs. modeling sets')
 ax1.imshow(z_array, extent=[-240,240, -50, 280],alpha=0.2)
 ax1.get_xaxis().set_visible(False)
+ax1.get_yaxis().set_visible(False)
 
 for j in range(1,len(z)):
     if z[j] > 49.999:
