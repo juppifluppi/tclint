0d006f35cf73e7d46d56990c231048635ae1d30a
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-11-30 15:42:40 +0000

    Update tclint_streamlit.py

--- tclint_streamlit.py
+++ tclint_streamlit.py
@@ -205,7 +205,7 @@ b, a = np.polyfit(go1, go2, deg=1)
 xseq = np.linspace(-4, 5.5, num=2)
 ax.plot(xseq, a + b * xseq, color="grey", linewidth=10, alpha=0.1,zorder=1)
 knn=ax.scatter(xseq, a + b * xseq, color="grey", s=30, alpha=0.1,edgecolors="grey", marker="s",zorder=1)
-ax.legend((l,m,p,w,o,n,knn),("Training set", "≥ 50 % probability", "Validation set", "≥ 50 % probability", "Compound", "≥ 50 % probability", "Wrong classification", "50 % probability"), ncol=1)
+ax.legend((l,m,p,w,o,n,knn),("Training set", "≥ 50 % probability", "Validation set", "≥ 50 % probability", "Compound", "Wrong classification", "50 % probability"), ncol=1)
 plt.show()
 st.pyplot(fig)
 
