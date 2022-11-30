7916d2c6a1b3539917aebdb8ec5f304802b4b234
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-11-30 12:40:42 +0000

    Update tclint_streamlit.py

--- tclint_streamlit.py
+++ tclint_streamlit.py
@@ -245,7 +245,7 @@ if tcl3*100 >= 50:
 b, a = np.polyfit(go1, go2, deg=1)
 xseq = np.linspace(-4, 5.5, num=100)
 knn=ax.scatter(xseq, a + b * xseq, color="grey", s=100, alpha=0.1)
-ax.legend((l,m,p,w,o,n,knn),("Training set", ">= 50 % probability", "Validation set", ">= 50 % probability", "Compound", "Wrong classification", "50 % probability"))
+ax.legend((l,m,p,w,o,n,knn),("Training set", "≥ 50 % probability", "Validation set", ">= 50 % probability", "Compound", "Wrong classification", "50 % probability"))
 plt.show()
 st.pyplot(fig)
 
