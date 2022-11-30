ff4a9f8e44fde5a269e82ac5e7afbb8081c040d8
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-11-30 12:04:05 +0000

    Update tclint_streamlit.py

--- tclint_streamlit.py
+++ tclint_streamlit.py
@@ -221,19 +221,19 @@ for j in range(0,len(z)):
     if z[j] >= 50:
         ax.scatter(x[j],y[j], color='b',alpha=0.1,s=200)
         if e1[j] == 0:
-            ax.scatter(x[j],y[j], color='yellow',alpha=1,s=5,marker="*")
+            ax.scatter(x[j],y[j], color='y',alpha=1,s=20,marker="*",zorder=2)
     if z[j] < 50:
         if e1[j] == 1:
-            ax.scatter(x[j],y[j], color='yellow',alpha=1,s=5,marker="*")
+            ax.scatter(x[j],y[j], color='y',alpha=1,s=20,marker="*",zorder=2)
             
 for j in range(0,len(z2)):
     if z2[j] >= 50:
         ax.scatter(x2[j],y2[j], color='r',alpha=0.1,s=200)
         if e2[j] == 0:
-            ax.scatter(x2[j],y2[j], color='y',alpha=1,s=5,marker="*",zorder=2)
+            ax.scatter(x2[j],y2[j], color='y',alpha=1,s=20,marker="*",zorder=2)
     if z2[j] < 50:
         if e2[j] == 1:
-            ax.scatter(x2[j],y2[j], color='y',alpha=1,s=5,marker="*",zorder=2)
+            ax.scatter(x2[j],y2[j], color='y',alpha=1,s=20,marker="*",zorder=2)
         
 if tcl3*100 >= 50:
     ax.scatter(logd,mr, color='g',alpha=0.1,s=250)
