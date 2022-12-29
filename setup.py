462a685de7f1ee38b7b568842a41c05bb9070e21
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2022-12-29 11:19:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-__version__ = "1.3"
+__version__ = "1.4"
 
 # Load README
 with open('README.md', encoding='utf-8') as f:
@@ -8,7 +8,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='tclint',
-    version='1.3',
+    version='1.4',
     author='Josef Kehrein',
     author_email='josef.kehrein@helsinki.fi',
     license='MIT',
