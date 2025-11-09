504fa533ad7f9e09151bd516b332762f78c8bb28
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2025-11-09 19:48:31 +0100

    Create redirect.py

---
+++ redirect.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="tclint")
+
+st.markdown("""
+## 🚀 We've moved!
+Please visit our new app muctcl 👉 [address](https://kehrein.xyz/muctcl/)!
+""")
